eb89ae447fc6a5a0fdcbf23ad708dca6acafba07
Benjamin Hipple <bhipple@bloomberg.net>
2015-01-24 19:42:23 -0500

    Re-ran script

--- mail_ip.sh
+++ mail_ip.sh
@@ -3,8 +3,8 @@
 # setup msg
 IP=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
 
-eval "sed -i 's/\(Subject: \).*$/\1$IP/g' msg.txt"
+eval "sed -i 's/\(Subject: \).*$/\1$IP/g' /home/smilodon/ip/msg.txt"
 
 # send it
-ssmtp benjamin.hipple@gmail.com < msg.txt
-ssmtp Chris.Hipple@gmail.com < msg.txt
+ssmtp benjamin.hipple@gmail.com < /home/smilodon/ip/msg.txt
+ssmtp Chris.Hipple@gmail.com < /home/smilodon/ip/msg.txt
