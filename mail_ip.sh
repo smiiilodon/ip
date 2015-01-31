2ee436e5ff15b805dd6a43b91968c0824f038bd5
Jeehyun Kim <inneekim@gmail.com>
2015-01-31 13:39:11 -0500

    adds my email address

--- mail_ip.sh
+++ mail_ip.sh
@@ -8,3 +8,4 @@ eval "sed -i 's/\(Subject: \).*$/\1$IP/g' /home/smilodon/ip/msg.txt"
 # send it
 ssmtp benjamin.hipple@gmail.com < /home/smilodon/ip/msg.txt
 ssmtp Chris.Hipple@gmail.com < /home/smilodon/ip/msg.txt
+ssmtp inneekim@gmail.com < /home/smilodon/ip/msg.txt
